182ae781a8d4b6854f71faa862cc44dcb48f9ad2
DatBongDem <se181844nguyentiendat@gmail.com>
2026-06-24 13:08:46 +0700

    check payment for edu

--- BusinessLogic/Services/PaymentService.cs
+++ BusinessLogic/Services/PaymentService.cs
@@ -87,6 +87,8 @@ namespace BusinessLogic.Services
                         plan.Name,
                         plan.Price);
 
+            string qrImageUrl = $"https://img.vietqr.io/image/{paymentLinkResult.Bin}-{paymentLinkResult.AccountNumber}-qr_only.png?amount={(long)plan.Price}&addInfo={Uri.EscapeDataString(paymentLinkResult.Description)}&accountName={Uri.EscapeDataString(paymentLinkResult.AccountName)}";
+
             return new CreatePaymentResponseDto
             {
                 QrCode = paymentLinkResult.QrCode,
@@ -96,7 +98,9 @@ namespace BusinessLogic.Services
                 Description = paymentLinkResult.Description,
                 TransactionCode = transactionCode,
                 Amount = plan.Price,
-                PlanName = plan.Name
+                PlanName = plan.Name,
+                CheckoutUrl = paymentLinkResult.CheckoutUrl,
+                QrImageUrl = qrImageUrl
             };
         }
 
